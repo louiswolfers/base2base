bf9266a284acba861f23b39808793c25691f871f
Louis Wolfers <louis.wolfers@gmail.com>
2018-01-04 12:59:05 +0100

    base2base skeleton made

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-function base8(base2) {
-	
-}
+var vm = new Vue({
+	el: '#app',
+	data: {
+		base2number: "0",
+		base8number: "0",
+		base10number: "0",
+		base16number: "0"
+	},
+	methods: {
+		base2(){
+			if(this.base2number != ""){
+				this.base10number = parseInt(this.base2number.toString(), 2);
+				this.base8number = this.base10number.toString(8);
+				this.base16number = this.base10number.toString(16);
+			}
+		},
+		base8(){
+		},
+		base10(){
+		},
+		base16(){
+		}
+	}
+});
