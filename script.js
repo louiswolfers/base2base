8932b3ab1e70c055de11b8cdbafb97227cb63a43
Louis Wolfers <louis.wolfers@gmail.com>
2018-01-04 13:19:34 +0100

    If input is blank, other inputs are equal to zero

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ var vm = new Vue({
 				this.base10number = parseInt(this.base2number, 2);
 				this.base8number = this.base10number.toString(8);
 				this.base16number = this.base10number.toString(16);
+			} else {
+				this.base8number = 0;
+				this.base10number = 0;
+				this.base16number = 0;
 			}
 		},
 		base8(){
@@ -19,19 +23,33 @@ var vm = new Vue({
 				this.base10number = parseInt(this.base8number, 8);
 				this.base2number = this.base10number.toString(2);
 				this.base16number = this.base10number.toString(16);
+			} else {
+				this.base2number = 0;
+				this.base10number = 0;
+				this.base16number = 0;
 			}
 		},
 		base10(){
-			this.base10number = parseInt(this.base10number);
-			this.base2number = this.base10number.toString(2);
-			this.base8number = this.base10number.toString(8);
-			this.base16number = this.base10number.toString(16);
+			if(this.base10number != ""){
+				this.base10number = parseInt(this.base10number);
+				this.base2number = this.base10number.toString(2);
+				this.base8number = this.base10number.toString(8);
+				this.base16number = this.base10number.toString(16);
+			} else {
+				this.base2number = 0;
+				this.base8number = 0;
+				this.base16number = 0;
+			}
 		},
 		base16(){
 			if(this.base16number != ""){
 				this.base10number = parseInt(this.base16number, 16);
 				this.base2number = this.base10number.toString(2);
 				this.base8number = this.base10number.toString(8);
+			} else {
+				this.base2number = 0;
+				this.base8number = 0;
+				this.base10number = 0;
 			}
 		}
 	}
