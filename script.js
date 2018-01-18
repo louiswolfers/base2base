aa05ece7f948fcbbbbeae70ae68e0b53182431e2
Louis Wolfers <louis.wolfers@gmail.com>
2018-01-18 19:59:49 +0100

    User can't type characters that are not from the base that he is writing in anymore

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
 var vm = new Vue({
 	el: '#app',
 	data: {
-		base2number: "0",
-		base8number: "0",
-		base10number: "0",
-		base16number: "0"
+		base2number: "",
+		base8number: "",
+		base10number: "",
+		base16number: "",
+		unsigned: false
 	},
 	methods: {
-		baseConverter(baseOrigin, baseName) {
-			this.base10number = parseInt(this[baseName], baseOrigin);
-			this.base2number = this.base10number.toString(2);
-			this.base8number = this.base10number.toString(8);
-			this.base16number = this.base10number.toString(16);
-		},
-		numberChecker(baseOrigin){
-			baseName = "base" + baseOrigin + "number";
-			if(this[baseName] == ""){
-				cleanInputFields();
-			} else if (isNaN(parseInt(this[baseName], baseOrigin))){
-				if(this[baseName].length == 1){
-					cleanInputFields();
-				} else {
-					this[baseName].pop();
-				}
+		baseConverter(baseOrigin, baseName, unsigned) {
+			if (!unsigned) {
+				this.base10number = parseInt(this[baseName], baseOrigin);
+				this.base2number = this.base10number.toString(2);
+				this.base8number = this.base10number.toString(8);
+				this.base16number = this.base10number.toString(16);
 			} else {
-				this.baseConverter(baseOrigin, baseName);
+				this.base10number = parseInt(this[baseName], baseOrigin);
+				this.base2number = this.base10number.toString(2).shift();
+				this.base8number = this.base10number.toString(8);
+				this.base16number = this.base10number.toString(16);
 			}
 		},
 		cleanInputFields(){
@@ -32,6 +26,53 @@ var vm = new Vue({
 			this.base8number = "";
 			this.base10number = "";
 			this.base16number = "";
+		},
+		correctInputFields(baseOrigin, baseName, unsigned){
+			if (unsigned) {
+				switch(baseOrigin) {
+					case 2:
+						this[baseName] = this[baseName].replace(/[^01]/g, "");
+						break;
+					case 8:
+						this[baseName] = this[baseName].replace(/([^0-7]|[^-])/g, "");
+						break;
+					case 10:
+						this[baseName] = this[baseName].replace(/([^0-9]|[^-])/g, "");
+						break;
+					case 16:
+						this[baseName] = this[baseName].replace(/([^0-9a-fA-f]|[^-])/g, "");
+						break;
+				}
+			} else {
+				switch(baseOrigin) {
+					case 2:
+						this[baseName] = this[baseName].replace(/[^01]/g, "");
+						break;
+					case 8:
+						this[baseName] = this[baseName].replace(/[^0-7]/g, "");
+						break;
+					case 10:
+						this[baseName] = this[baseName].replace(/[^0-9]/g, "");
+						break;
+					case 16:
+						this[baseName] = this[baseName].replace(/[^0-9a-fA-f]/g, "");
+						break;
+				}
+			}
+		},
+		numberChecker(baseOrigin){
+			baseName = "base" + baseOrigin + "number";
+			if(this[baseName] == ""){
+				this.cleanInputFields();
+			} else if (isNaN(parseInt(this[baseName], baseOrigin))){
+				if(this[baseName].length == 1){
+					this.cleanInputFields();
+				} else {
+					this.correctInputFields(baseOrigin, baseName);
+				}
+			} else {
+				this.baseConverter(baseOrigin, baseName);
+			}
 		}
 	}
 });
