f9e997ad926f64dce24c582e8aa799c0875d329a
Louis Wolfers <louis.wolfers@gmail.com>
2018-01-23 08:42:05 +0100

    I believe I will not be able to the unsigned feature, as the parseInt() function removes the first zeroes

--- script.js
+++ script.js
@@ -4,22 +4,14 @@ var vm = new Vue({
 		base2number: "",
 		base8number: "",
 		base10number: "",
-		base16number: "",
-		unsigned: false
+		base16number: ""
 	},
 	methods: {
-		baseConverter(baseOrigin, baseName, unsigned) {
-			if (!unsigned) {
-				this.base10number = parseInt(this[baseName], baseOrigin);
-				this.base2number = this.base10number.toString(2);
-				this.base8number = this.base10number.toString(8);
-				this.base16number = this.base10number.toString(16);
-			} else {
-				this.base10number = parseInt(this[baseName], baseOrigin);
-				this.base2number = this.base10number.toString(2).shift();
-				this.base8number = this.base10number.toString(8);
-				this.base16number = this.base10number.toString(16);
-			}
+		baseConverter(baseOrigin, baseName) {
+			this.base10number = parseInt(this[baseName], baseOrigin);
+			this.base2number = this.base10number.toString(2);
+			this.base8number = this.base10number.toString(8);
+			this.base16number = this.base10number.toString(16);
 		},
 		cleanInputFields(){
 			this.base2number = "";
@@ -27,37 +19,20 @@ var vm = new Vue({
 			this.base10number = "";
 			this.base16number = "";
 		},
-		correctInputFields(baseOrigin, baseName, unsigned){
-			if (unsigned) {
-				switch(baseOrigin) {
-					case 2:
-						this[baseName] = this[baseName].replace(/[^01]/g, "");
-						break;
-					case 8:
-						this[baseName] = this[baseName].replace(/([^0-7]|[^-])/g, "");
-						break;
-					case 10:
-						this[baseName] = this[baseName].replace(/([^0-9]|[^-])/g, "");
-						break;
-					case 16:
-						this[baseName] = this[baseName].replace(/([^0-9a-fA-f]|[^-])/g, "");
-						break;
-				}
-			} else {
-				switch(baseOrigin) {
-					case 2:
-						this[baseName] = this[baseName].replace(/[^01]/g, "");
-						break;
-					case 8:
-						this[baseName] = this[baseName].replace(/[^0-7]/g, "");
-						break;
-					case 10:
-						this[baseName] = this[baseName].replace(/[^0-9]/g, "");
-						break;
-					case 16:
-						this[baseName] = this[baseName].replace(/[^0-9a-fA-f]/g, "");
-						break;
-				}
+		correctInputFields(baseOrigin, baseName){
+			switch(baseOrigin) {
+				case 2:
+					this[baseName] = this[baseName].replace(/[^01]/g, "");
+					break;
+				case 8:
+					this[baseName] = this[baseName].replace(/([^0-7]|[^-])/g, "");
+					break;
+				case 10:
+					this[baseName] = this[baseName].replace(/([^0-9]|[^-])/g, "");
+					break;
+				case 16:
+					this[baseName] = this[baseName].replace(/([^0-9a-fA-f]|[^-])/g, "");
+					break;
 			}
 		},
 		numberChecker(baseOrigin){
